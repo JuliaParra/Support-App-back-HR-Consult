7c810e558736be3bb12608cdef78c506489b2a11
JuliaParra <musicaparatodo770@gmail.com>
2024-07-26 02:19:55 +0200

    primer proyecto spring boot

---
+++ src/test/java/dev/julia/support/app/SupportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.julia.support.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
